0179c2b79cc058023ce0eb66f0b6882750ed2f92
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-08-03 16:38:47 -0400

    i/o r,w,a file

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode = 'a', encoding = 'utf-8') as logfile:
+        logfile.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode = 'a', encoding = 'utf-8') as logfile:
+        logfile.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt",mode = 'r', encoding = 'utf-8') as logfile:
+        return logfile.read()
+
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+read_file(file_name="logfile")
